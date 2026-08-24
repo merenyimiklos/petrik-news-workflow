be037639386c2c0c22dd216d4a6f9803c6c9aac0
Merényi Miklós <89610165+merenyimiklos@users.noreply.github.com>
2026-08-25 00:17:08 +0200

    Enable GitHub-native updater

--- petrik-news-workflow.php
+++ petrik-news-workflow.php
@@ -3,7 +3,7 @@
  * Plugin Name: Petrik News Workflow
  * Plugin URI:  https://github.com/merenyimiklos/petrik-news-workflow
  * Description: Belső hírbeküldési és vezetői jóváhagyási workflow a Petrik WordPress oldalához.
- * Version:     1.0.8-test
+ * Version:     1.0.9-test
  * Author:      Petrik
  * License:     GPL-2.0-or-later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
@@ -16,7 +16,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-define( 'PNW_VERSION', '1.0.8-test' );
+define( 'PNW_VERSION', '1.0.9-test' );
 define( 'PNW_FILE', __FILE__ );
 define( 'PNW_DIR', plugin_dir_path( __FILE__ ) );
 define( 'PNW_URL', plugin_dir_url( __FILE__ ) );
@@ -39,12 +39,14 @@ require_once PNW_DIR . 'includes/class-pnw-test-mode.php';
 require_once PNW_DIR . 'includes/class-pnw-access.php';
 require_once PNW_DIR . 'includes/class-pnw-actions.php';
 require_once PNW_DIR . 'includes/class-pnw-frontend-login.php';
+require_once PNW_DIR . 'includes/class-pnw-updater.php';
 require_once PNW_DIR . 'includes/class-pnw-frontend.php';
 require_once PNW_DIR . 'includes/class-pnw-admin.php';
 require_once PNW_DIR . 'includes/class-pnw-plugin.php';
 
 PNW_Test_Mode::init();
 PNW_Frontend_Login::init();
+PNW_Updater::init();
 
 register_activation_hook( __FILE__, array( 'PNW_Plugin', 'activate' ) );
 register_deactivation_hook( __FILE__, array( 'PNW_Plugin', 'deactivate' ) );
