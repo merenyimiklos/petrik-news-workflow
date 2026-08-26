2c3929a4c521608acaa124f0c07f744a25cac240
Merényi Miklós <89610165+merenyimiklos@users.noreply.github.com>
2026-08-26 14:34:27 +0200

    Remove buggy TinyMCE link controls

--- includes/trait-pnw-frontend-editor.php
+++ includes/trait-pnw-frontend-editor.php
@@ -171,7 +171,7 @@ trait PNW_Frontend_Editor_Trait {
                 'tinymce'          => array(
                     'menubar'  => false,
                     'statusbar' => false,
-                    'toolbar1' => 'formatselect,bold,italic,bullist,numlist,blockquote,alignleft,aligncenter,alignright,link,unlink,undo,redo,removeformat,fullscreen',
+                    'toolbar1' => 'formatselect,bold,italic,bullist,numlist,blockquote,alignleft,aligncenter,alignright,undo,redo,removeformat',
                     'toolbar2' => '',
                 ),
             )
